9521182b8c167a285c634728e9901086bd15d92d
Moroj-Alharbi <moroj.alh@gmail.com>
2019-01-15 14:41:57 +0300

    edit the models files with the group

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -14,10 +14,11 @@ const sendClient =  (req, res) => res.json ({ clinets: res.locals.clients});
 const sendProducts = (req, res) => res.json ({products : res.locals.products});
 const sendProduct = (req, res) => res.json ({products : res.locals.products});
 const sendrequests =(req, res) => res.json({request: res.locals.requests});
-const sendrequest =(req, res) => res.json({request: res.locals.requests});
+const sendrequest =(req, res) => res.json({request: res.locals.request});
 const sendSuccess = (req, res) => res.json({ message: 'success' });
-// const sendreqProducts = (req, res) => res.json({reqProducts: res.locals.reqProducts});
-// const sendreqProduct = (req, res) => res.json({reqProduct: res.locals.reqProducts});
+
+const sendreqProducts = (req, res) => res.json({reqProducts: res.locals.reqProducts});
+const sendreqProduct = (req, res) => res.json({reqProduct: res.locals.reqProduct});
 
 router.get('/clients', clients.getAll, sendClient);
 router.post('/clients',  clients.create, sendClients);
@@ -33,8 +34,8 @@ router.post('/requests', requests.create, sendrequest);
 router.put('/requests/:id', requests.update, sendrequest);
 router.delete('/requests/:id', requests.delete, sendSuccess);
 
-// router.get('/reqProduct', reqProduct.getByReqId, sendreqProducts);
-// router.post('/reqProduct', reqProduct.create, sendreqProduct);
-// router.delete('/reqProduct/:id', reqProduct.delete, sendSuccess);
+router.get('/reqProduct', reqProduct.getByReqId, sendreqProducts);
+router.post('/reqProduct', reqProduct.create, sendreqProduct);
+router.delete('/reqProduct/:id', reqProduct.delete, sendSuccess);
 
 module.exports = router;
